51c67ecada526be62ee1534e48206ad1ec408c8c
panghaibin <i@hbte.ch>
2023-02-04 01:03:28 +0800

    almost done

--- src/index.js
+++ src/index.js
@@ -1,15 +1,92 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npx wrangler dev src/index.js` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npx wrangler publish src/index.js --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+const blockedCountry = ['JP'];
+const allowedIP = ['127.0.0.1'];
 
-export default {
-	async fetch(request, env, ctx) {
-		return new Response("Hello World!");
-	},
-};
+
+class Response_ {
+  static redirect(url) {
+    return new Response(null, {
+      status: 302,
+      headers: {
+        Location: url,
+      },
+    });
+  }
+
+  static redirectForever(url) {
+    return new Response(null, {
+      status: 301,
+      headers: {
+        Location: url,
+      },
+    });
+  }
+
+  static notFound() {
+    return new Response('Not Found', {
+      status: 404,
+    });
+  }
+
+  static forbidden(msg = 'Forbidden') {
+    return new Response(msg, {
+      status: 403,
+    });
+  }
+
+  static html(data) {
+    return new Response(data, {
+      headers: {
+        'Content-Type': 'text/html;charset=UTF-8',
+      },
+    });
+  }
+
+  static json(data) {
+    return new Response(JSON.stringify(data), {
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8',
+      },
+    });
+  }
+
+  static js(data) {
+    return new Response(data, {
+      headers: {
+        'Content-Type': 'application/javascript;charset=UTF-8',
+      },
+    });
+  }
+}
+
+async function supportFetch(pathname) {
+  const url = `https://support.casio.com${pathname}`;
+  const res = await fetch(url);
+  if (!res.ok) {
+    return Response_.notFound();
+  }
+  return res;
+}
+
+async function handleRequest(request) {
+  const country = request.cf.country;
+  const ip = request.headers.get('CF-Connecting-IP');
+  if (blockedCountry.indexOf(country) !== -1 && allowedIP.indexOf(ip) === -1) {
+    return Response_.forbidden('Access Denied');
+  }
+
+  const { hostname, pathname } = new URL(request.url);
+
+	if (['support.casio.com.caduo.ml', 'support.casio.caduo.ml'].indexOf(hostname) !== -1) {
+		return Response_.redirectForever('https://support.caduo.ml' + pathname);
+	}
+
+	if (pathname === '/') {
+		return Response_.html('Hello World');
+	}
+
+  return supportFetch(pathname);
+}
+
+addEventListener('fetch', function (event) {
+  event.respondWith(handleRequest(event.request));
+});
