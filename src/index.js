31662c09867ac1ab42c736b549cd562b599d311a
panghaibin <i@hbte.ch>
2023-02-04 14:31:51 +0800

    refactor

--- src/index.js
+++ src/index.js
@@ -58,36 +58,58 @@ class Response_ {
   }
 }
 
-async function supportFetch(request) {
-  const { href, origin } = new URL(request.url);
-  const url = href.replace(origin, 'https://support.casio.com');
-  const res = await fetch(url);
+class CasioProxy {
+  static async handleRequest(request) {
+    const { hostname } = new URL(request.url);
+    const subDomain = hostname.split('.')[0];
 
-  if (!res.ok) {
+    if (['support', 'world'].indexOf(subDomain) !== -1) {
+      return await this.handleGeneralRequest(request);
+    }
+
     return Response_.notFound();
   }
 
-  if (res.url !== url) {
-    return Response_.redirect(res.url.replace('https://support.casio.com', origin));
+  static removeRubbish(html) {
+    html = html.replace(/<!-- Google Tag Manager -->[\s\S]*?<!-- End Google Tag Manager -->/g, '<!-- 01 -->');
+    html = html.replace(/<!-- Google Tag Manager \(noscript\) -->[\s\S]*?<!-- End Google Tag Manager \(noscript\) -->/g, '<!-- 02 -->');
+    html = html.replace(/<!-- Adobe analytics Tag -->[\s\S]*?<!-- END Adobe analytics Tag -->/g, '<!-- 03 -->');
+    html = html.replace(/<script src="\/\/assets\.adobedtm\.com\/[\s\S]*?<\/script>/g, '<!-- 04 -->');
+    html = html.replace(/\(function[\s\S]*?gtm\.js[\s\S]*?<\/script>/g, '</script>');
+    html = html.replace(/<noscript>[\s\S]*?<iframe[\s\S]*?googletagmanager[\s\S]*?<\/noscript>/g, '<!-- 05 -->');
+    return html;
   }
 
-  const contentType = res.headers.get('Content-Type');
-  if (contentType.indexOf('text/html') === -1) {
+  static URLRewrite(res) {
+    res = res.replace(/https?:\/\/support\.casio\.com/g, 'https://support.caduo.ml');
+    res = res.replace(/https?:\/\/world\.casio\.com/g, 'https://world.caduo.ml');
     return res;
   }
 
-  let html = await res.text();
-  html = html.replace(/https:\/\/support\.casio\.com/g, '');
+  static async handleGeneralRequest(request) {
+    const { href, origin, hostname } = new URL(request.url);
+    const subDomain = hostname.split('.')[0];
+    const url = href.replace(origin, `https://${subDomain}.casio.com`);
+    const res = await fetch(url);
 
-  /* remove rubbish */
-  html = html.replace(/<!-- Google Tag Manager -->[\s\S]*?<!-- End Google Tag Manager -->/g, '<!-- 01 -->');
-  html = html.replace(/<!-- Google Tag Manager \(noscript\) -->[\s\S]*?<!-- End Google Tag Manager \(noscript\) -->/g, '<!-- 02 -->');
-  html = html.replace(/<!-- Adobe analytics Tag -->[\s\S]*?<!-- END Adobe analytics Tag -->/g, '<!-- 03 -->');
-  html = html.replace(/<script src="\/\/assets\.adobedtm\.com\/[\s\S]*?<\/script>/g, '<!-- 04 -->');
-  html = html.replace(/\(function[\s\S]*?gtm\.js[\s\S]*?<\/script>/g, '</script>');
-  html = html.replace(/<noscript>[\s\S]*?<iframe[\s\S]*?googletagmanager[\s\S]*?<\/noscript>/g, '<!-- 05 -->');
+    if (!res.ok) {
+      return Response_.notFound('Origin Error');
+    }
 
-  return Response_.html(html);
+    if (res.url !== url) {
+      return Response_.redirect(this.URLRewrite(res.url));
+    }
+
+    const contentType = res.headers.get('Content-Type');
+    if (contentType.indexOf('text/html') === -1) {
+      return res;
+    }
+
+    let html = await res.text();
+    html = this.URLRewrite(html);
+    html = this.removeRubbish(html);
+    return Response_.html(html);
+  }
 }
 
 async function handleRequest(request) {
@@ -97,17 +119,18 @@ async function handleRequest(request) {
     return Response_.forbidden('Access Denied');
   }
 
-  const { protocol, hostname, pathname, href, origin } = new URL(request.url);
+  const { protocol, hostname, href, origin } = new URL(request.url);
 
-  if (['support.casio.com.caduo.ml', 'support.casio.caduo.ml'].indexOf(hostname) !== -1 || (protocol !== 'https:' && allowedIP.indexOf(ip) === -1)) {
-    return Response_.redirect(href.replace(origin, 'https://support.caduo.ml'));
+  if (protocol !== 'https:' && allowedIP.indexOf(ip) === -1 && hostname.indexOf('casio') === -1) {
+    return Response_.redirect(href.replace(protocol, 'https:'));
   }
 
-  if (pathname === '/') {
-    return Response_.html('Hello World');
+  if (hostname.indexOf('casio') !== -1) {
+    const subDomain = hostname.split('.')[0];
+    return Response_.redirect(href.replace(origin, `https://${subDomain}.caduo.ml`));
   }
 
-  return supportFetch(request);
+  return await CasioProxy.handleRequest(request);
 }
 
 addEventListener('fetch', function (event) {
