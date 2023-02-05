dce228dd151a9c7f1f2f9a79a905c3d79bf95d3b
panghaibin <i@hbte.ch>
2023-02-05 12:21:33 +0800

    refactor

--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ const blockedCountry = ['JP'];
 const allowedIP = ['127.0.0.1'];
 
 async function handleRequest(request) {
+  const { protocol, hostname, pathname, href, origin } = new URL(request.url);
+
+  if (pathname === '/robots.txt') {
+    return Response_.text('User-agent: *\nDisallow: /');
+  }
+
   const country = request.cf.country;
   const ip = request.headers.get('CF-Connecting-IP');
   if (blockedCountry.indexOf(country) !== -1 && allowedIP.indexOf(ip) === -1) {
     return Response_.forbidden('Access Denied');
   }
-
-  const { protocol, hostname, pathname, href, origin } = new URL(request.url);
 
   if (protocol !== 'https:' && allowedIP.indexOf(ip) === -1 && hostname.indexOf('casio') === -1) {
     return Response_.redirect(href.replace(protocol, 'https:'));
@@ -20,10 +24,6 @@ async function handleRequest(request) {
   if (hostname.indexOf('casio') !== -1) {
     const subDomain = hostname.split('.')[0];
     return Response_.redirect(href.replace(origin, `https://${subDomain}.caduo.ml`));
-  }
-
-  if (pathname === '/robots.txt') {
-    return Response_.text('User-agent: *\nDisallow: /');
   }
 
   return await CasioProxy.handleRequest(request);
